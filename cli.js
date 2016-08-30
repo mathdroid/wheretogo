67c1f7ace1cf08dbc0364ae2ace2fc88a3ee7881
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2016-08-30 22:36:31 +0700

    first

---
+++ cli.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+'use strict';
+const dns = require('dns');
+const readline = require('readline');
+const meow = require('meow');
+const logUpdate = require('log-update');
+const chalk = require('chalk');
+const debounce = require('lodash.debounce');
+const hasAnsi = require('has-ansi');
+const mem = require('mem');
+const moment = require('moment');
+const wheretogo = require('./');
+
+// limit it to 7 results so not to overwhelm the user
+// this also reduces the chance of showing unrelated emojis
+const fetch = mem(str => wheretogo(str).then(arr => arr.map(e => e ? `${chalk.green(moment(e.start).fromNow())}: ${chalk.bold(e.n)}\n${chalk.dim(JSON.stringify(e.d))}\nLink: ${e.tktU ? e.tktU : 'no Ticket Link available'}` : 'No Events Available').join('\n\n')));
+
+const debouncer = debounce(cb => cb(), 200);
+
+const cli = meow(`
+	Usage
+	  $ wheretogo [text]
+
+	Example
+	  $ wheretogo 'bandung'
+	  [eventResults]
+
+	Run it without arguments to enter the live search
+`);
+
+if (cli.input.length > 0) {
+	fetch(cli.input[0]).then(console.log);
+	return;
+}
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+const pre = `\n${chalk.bold('Where you at?')}\n${chalk.bold.cyan('›')} `;
+const query = [];
+let prevResult = '';
+
+dns.lookup('a.omgwhen.com', err => {
+	if (err && err.code === 'ENOTFOUND') {
+		logUpdate(`\n${chalk.bold.red('› ')}${chalk.dim('Please check your internet connection')}\n\n`);
+		process.exit(1);
+	} else {
+		logUpdate(`${pre}${chalk.dim('Relevant events will appear when you start writing')}\n\n`);
+	}
+});
+
+process.stdin.on('keypress', (ch, key) => {
+	key = key || {};
+
+	if (hasAnsi(key.sequence)) {
+		return;
+	}
+
+	if (key.name === 'escape' || (key.ctrl && key.name === 'c')) {
+		if (query.length <= 1) {
+			logUpdate();
+			readline.moveCursor(process.stdout, 0, -1);
+		}
+
+		process.exit();
+	}
+
+	if (key.name === 'backspace') {
+		query.pop();
+	} else if (key.name === 'return' || (key.ctrl && key.name === 'u')) {
+		query.length = 0;
+	} else {
+		query.push(ch);
+	}
+
+	const queryStr = query.join('');
+
+	logUpdate(`${pre}${chalk.bold(queryStr)}\n${prevResult}\n`);
+
+	if (query.length <= 1) {
+		prevResult = '';
+		logUpdate(`${pre}${chalk.bold(queryStr)}\n\n`);
+		return;
+	}
+
+	debouncer(() => {
+		fetch(queryStr).then(events => {
+			if (query.length <= 1) {
+				return;
+			}
+
+			prevResult = events;
+			logUpdate(`${pre}${chalk.bold(query.join(''))}\n${events}\n`);
+		});
+	});
+});
---
+++ index.js
@@ -0,0 +1,68 @@
+'use strict';
+const got = require('got');
+const promisify = require('promisify-node');
+const geocoder = require('geocoder');
+
+// module.exports = str => got('emoji.getdango.com/api/emoji', {
+// 	json: true,
+// 	query: {
+// 		q: str
+// 	}
+// }).then(res => res.body.results.map(x => x.text));
+
+// "SearchFilters":{
+//   "hoursAhead":5,
+//   "tree":{"Sub":{"Popular":{}}},
+//   "radius":5,
+//   "time":"${newDate()}",
+//   "googleGeoObject":${googleGeoObject},
+//   "ongoingHours":0,
+//   "promoted":false}}`,
+
+function getGeocode (str, callback) {
+  geocoder.geocode(str, callback)
+}
+let wrap = promisify(getGeocode)
+
+function newDate () {
+  let timeStr = new Date()
+  // timeStr.setHours(4, 0, 0, 0)
+  console.log(timeStr.toISOString())
+  return timeStr.toISOString()
+}
+
+module.exports = str => wrap(str).then(res => {
+  console.dir(res.results[0])
+  return res.results.map(place => JSON.stringify(place))
+}).then(googleGeoObject => got.post('https://a.omgwhen.com/Yoosh/YooshFeService/json2/getEventsByGeoEntity', {
+  headers: {
+    'Origin': 'https://omgwhen.com',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.8,id;q=0.6,ms;q=0.4',
+    'Authorization': 'Yoo-sess null',
+    'Content-Type': 'application/json',
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+    'Cache-Control': 'max-age=0',
+    'Referer': 'https://omgwhen.com/',
+    'Connection': 'keep-alive',
+  },
+  body: `{
+    "APIKey":"1.0.0___3659E990-DDBB-42F6-B9DA-5E39B301FE74",
+    "DeviceType":2,
+    "DeviceId":null,
+    "RequestingUserId":"00000000-0000-0000-0000-000000000001",
+    "SearchFilters":{
+      "hoursAhead":5,
+      "radius":5,
+      "time":"${newDate()}",
+      "googleGeoObject":${googleGeoObject},
+      "ongoingHours":0,
+      "promoted":false}}`,
+})).then(res => JSON.parse(res.body)).then(res => res.ResultSet).then(set => {
+  if (set.Events.length > 0) {
+    return set.Events
+  } else {
+    return [{n: "No events", d: "No Events Found"}]
+  }
+})
